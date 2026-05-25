42e2d7a32093c698a0b335b31536770c8aef7e4d
Tom Singleton <tomquil13@gmail.com>
2026-05-25 21:49:43 +0100

    correct app id

--- src/main.rs
+++ src/main.rs
@@ -56,7 +56,7 @@ fn main() {
         cx.open_window(
             WindowOptions {
                 window_bounds: Some(WindowBounds::Windowed(bounds)),
-                app_id: Some("com.bludio.app".to_string()),
+                app_id: Some("dev.toomosin.bludio".to_string()),
                 ..Default::default()
             },
             |window, cx| cx.new(|cx| app::BludioApp::new(window, cx)),
